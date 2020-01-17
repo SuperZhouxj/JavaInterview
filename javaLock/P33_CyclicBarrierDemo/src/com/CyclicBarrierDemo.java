cab9a3def79578dd6d2cb99feb34dab10dac133b
zhouxingjing <zxj5017@163.com>
2020-01-17 22:26:00 +0800

    P33 CyclicBarrierDemo

---
+++ javaLock/P33_CyclicBarrierDemo/src/com/CyclicBarrierDemo.java
@@ -0,0 +1,24 @@
+package com;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierDemo {
+    public static void main(String args[]){
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7,()->{System.out.println("*****召唤神龙");});
+        System.out.println("");
+        for(int i =1;i<=7;i++){
+            final int temp = i;
+            new Thread(()->{
+                System.out.println(Thread.currentThread().getName() + "\t 收集第"+ temp +"龙珠");
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            },String.valueOf(i)).start();
+        }
+    }
+}
